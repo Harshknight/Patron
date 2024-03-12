7e80a99834501368d4f326187d85a7c16d92301c
Harsh Ranjan <98806101+Harshknight@users.noreply.github.com>
2024-03-12 14:00:55 +0530

    Initial Commit

--- app/src/main/java/com/example/patron/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/patron/fragments/HomeFragment.kt
@@ -9,12 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.patron.Adapter.FollowAdapter
 import com.example.patron.Adapter.PostAdapter
 import com.example.patron.Model.Post
+import com.example.patron.Model.User
 import com.example.patron.R
 import com.example.patron.databinding.FragmentHomeBinding
+import com.example.patron.utils.FOLLOW
 import com.example.patron.utils.POST
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
 
@@ -23,6 +27,8 @@ class HomeFragment : Fragment() {
     private var postList= ArrayList<Post>()
     private lateinit var adapter: PostAdapter
 
+    private lateinit var followAdapter: FollowAdapter
+    private var followList = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,8 +47,23 @@ class HomeFragment : Fragment() {
         binding.postRv.layoutManager=LinearLayoutManager(requireContext())
         binding.postRv.adapter=adapter
 
+        followAdapter = FollowAdapter(requireContext(),followList)
+        binding.followRv.layoutManager=LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        binding.followRv.adapter=adapter
+
         setHasOptionsMenu(true)
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.materialToolbar2)
+
+        Firebase.firestore.collection(Firebase.auth.currentUser!!.uid+ FOLLOW).get().addOnSuccessListener {
+            var tempList=ArrayList<User>()
+            followList.clear()
+            for(i in it.documents){
+                var user :User = i.toObject<User>()!!
+                tempList.add(user)
+            }
+            followList.addAll(tempList)
+            followAdapter.notifyDataSetChanged()
+        }
 
         Firebase.firestore.collection(POST).get().addOnSuccessListener {
             var tempList=ArrayList<Post>()
